1a8074bd5f48e088af97a9b02c2343f06fd76f6a
Bartosz <bartosz.szopiera@gmail.com>
2017-09-02 17:44:42 +0200

    Responsive layout for 'allQuestionsPage' component.

---
+++ src/components/allQuestionsPage.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <app-header></app-header>
+    <pageBody>
+      <questionSummary></questionSummary>
+    </pageBody>
+  </div>
+</template>
+
+<script>
+import questionSummary from './questionSummary.vue';
+import header from './allQuestionsHeader.vue';
+import pageBody from './pageBody.vue';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    'questionSummary':  questionSummary,
+    'app-header':       header,
+    'pageBody':          pageBody,
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
