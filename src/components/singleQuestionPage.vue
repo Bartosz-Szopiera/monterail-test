c8dd2fc668531153bb303780bcbeff13115713d5
Bartosz <bartosz.szopiera@gmail.com>
2017-09-02 17:46:38 +0200

    Responsive layout for 'singleQuestionPage' component.

---
+++ src/components/singleQuestionPage.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div>
+    <app-header></app-header>
+    <pageBody>
+      <question></question>
+      <answer></answer>
+      <comment></comment>
+      <answer></answer>
+    </pageBody>
+  </div>
+</template>
+
+<script>
+import header from './singleQuestionHeader.vue';
+import pageBody from './pageBody.vue';
+import question from './questionDetails.vue';
+import answer from './answer.vue';
+import comment from './comment.vue';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    'app-header':     header,
+    'pageBody':       pageBody,
+    'question':       question,
+    'answer':         answer,
+    'comment':        comment,
+  }
+}
+</script>
+
+<style lang="css">
+</style>
