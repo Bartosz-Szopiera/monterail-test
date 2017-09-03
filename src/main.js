8dc9e12e0a5b8f1be02a00dfa060f4719e4bf50f
Bartosz <bartosz.szopiera@gmail.com>
2017-09-03 05:34:28 +0200

    Add router, event-bus and depndencies.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import routes from './routes';
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
+const bus = new Vue();
+
+export default bus;
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
