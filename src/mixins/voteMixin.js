27c61aa3cd7315f76259dee04f32f30386134af4
Bartosz <bartosz.szopiera@gmail.com>
2017-09-04 14:12:53 +0200

    Fix voteMixing.js by removing reference to non existent property.

--- src/mixins/voteMixin.js
+++ src/mixins/voteMixin.js
@@ -3,18 +3,17 @@ export default {
     vote(x, path) {
 
       const elType = Object.keys(this.$props)[2];
-      var votes = this[elType].votes;
-      console.log(elType);
-      console.log(this.comment.votes);
+      let votes = this[elType].votes;
+
       if (x<0 && votes === 0) return
 
       votes += x;
 
-      // const id = this.$route.params.id;
       const url = `https://monterail-123.firebaseio.com/questions/${path}/votes.json`;
       this.$http.put(url, votes).then(function(data){
         console.log(data);
       });
+
       this[elType].votes = votes
     }
   }
